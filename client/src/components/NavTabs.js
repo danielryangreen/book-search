0a19f7d9d7f1173c8842e81fe3a5bf9040d024c4
Dan Green <dan.ryan.green@gmail.com>
2021-06-13 21:25:24 -0600

    Add React Router and 2 pages

---
+++ client/src/components/NavTabs.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavTabs() {
+
+
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        Search
+      </li>
+      <li className="nav-item">
+        Saved
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
