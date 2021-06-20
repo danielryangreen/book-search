0e7f299c3b391c206134f316e745ce58c7d2c057
Dan Green <dan.ryan.green@gmail.com>
2021-06-19 19:20:20 -0600

    Add Hero component

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 function Saved() {
   return (
     <div>
+      <Hero backgroundImage="">
+        <h1>(React) Google Books Search</h1>
+        <h2>Search for and Save Books of Interest</h2>
+      </Hero>
       <h1>Saved</h1>
     </div>
   );
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 function Search() {
   return (
     <div>
+      <Hero backgroundImage="">
+        <h1>(React) Google Books Search</h1>
+        <h2>Search for and Save Books of Interest</h2>
+      </Hero>
       <h1>Search</h1>
     </div>
   );
