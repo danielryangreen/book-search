cfe7e01b3045250e3a7121af44215f5bc181c6ad
Dan Green <dan.ryan.green@gmail.com>
2021-06-19 19:57:20 -0600

    Change NavTabs to Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-import NavTabs from "./components/NavTabs";
+import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 // import logo from "./logo.svg";
 // import "./App.css";
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
+        <Navbar />
         <Wrapper>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
